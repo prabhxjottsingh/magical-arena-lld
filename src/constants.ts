64246d75474d83329e28281a2dc3d5c7f1cf531c
prabhxjottsingh <prabhx.jott@gmail.com>
2024-06-07 14:38:33 +0530

    handled InternalServerError as error response in constants file

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export enum constants {
+    SUCCESS_REPONSE = "success",
+    ERROR_REPONSE = "Internal Server Error"
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { constants } from "./constants";
 import { BattleHandler } from "./handler/battle-handler";
 import { MagicalArena } from "./modals/magical-arena";
 import { Player } from "./modals/player";
@@ -6,13 +7,38 @@ import { InitiateArenaBattleRequest } from "./modals/requests/initiate-arena-bat
 
 let battleHandler = new BattleHandler();
 
+const successResponse = (jsonResponse: any) => {
+    const response = {
+        body: "",
+        statusCode: 0
+    };
+    if (!jsonResponse) {
+        jsonResponse = constants.SUCCESS_REPONSE;
+    }
+    response.body = jsonResponse;
+    response.statusCode = 200;
+};
+
+const errorResponse = (error: any) => {
+    const response = {
+        body: "",
+        statusCode: 0
+    };
+    if (!error) {
+        error = constants.ERROR_REPONSE;
+    }
+    response.body = error;
+    response.statusCode = 500;
+}
+
 export const initiateArenaBattle = async (reqJson: string) => {
     try {
         const startBattleReq = new InitiateArenaBattleRequest(JSON.parse(reqJson));
         battleHandler.initiateBattle(startBattleReq);
+        successResponse(null);
     } catch (error) {
         console.error("Error in initiating the ArenaBattle: ", error);
-        throw error;
+        errorResponse(error);
     }
 
 };
